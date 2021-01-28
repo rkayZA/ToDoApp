4295107b1eff8e015adc9f91b60dbf429c2f1ced
Rob <robert.klaic@outlook.com>
2021-01-28 17:09:08 +0200

    Update to code
    Updated code to use  an ObservableCollection
    Added test data to populate the list
    Added some functionality to the "Completed" checkbox

--- ToDoApp.WPFUI/Controls/MainAppControl.xaml.cs
+++ ToDoApp.WPFUI/Controls/MainAppControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,9 +20,28 @@ namespace ToDoApp.WPFUI.Controls
     /// </summary>
     public partial class MainAppControl : UserControl
     {
+        ObservableCollection<ToDoItemModel> toDoItemList = new ObservableCollection<ToDoItemModel>();
+        
         public MainAppControl()
         {
             InitializeComponent();
+            PopulateItemList();
+        }
+
+        private void PopulateItemList()
+        {
+           
+            // Test data to load on application start - just simulating a load from database/file
+            for (int i = 1; i < 6; i++)
+            {
+                ToDoItemModel listItem = new ToDoItemModel();
+                listItem.DateAdded = DateTime.Now.ToString("dd-MM-yyyy");
+                listItem.ToDoItem = $"Item {i} - Thing I need to do";
+
+                toDoItemList.Add(listItem);
+            }
+
+            toDoItemListGrid.ItemsSource = toDoItemList;
         }
 
         private void addItemButton_Click(object sender, RoutedEventArgs e)
@@ -34,15 +54,32 @@ namespace ToDoApp.WPFUI.Controls
             ToDoItemModel toDoItem = new ToDoItemModel();
             toDoItem.DateAdded = DateTime.Now.ToString("dd-MM-yyyy");
             toDoItem.ToDoItem = toDoListItemText.Text;
-            toDoItemListGrid.Items.Add(toDoItem);
+            toDoItemList.Add(toDoItem);
 
             toDoListItemText.Clear();
         }
 
         private void deleteItemBtn_Click(object sender, RoutedEventArgs e)
         {
-            var itemindex = toDoItemListGrid.SelectedIndex;
-            toDoItemListGrid.Items.RemoveAt(toDoItemListGrid.SelectedIndex);
+            var selectedItem = toDoItemListGrid.SelectedItem as ToDoItemModel;
+            toDoItemList.Remove(selectedItem);
         }
+
+        public void OnChecked(object sender, RoutedEventArgs e)
+        {
+                        
+            var row = toDoItemListGrid.ItemContainerGenerator.ContainerFromItem(toDoItemListGrid.SelectedItem) as DataGridRow;
+            row.Background = Brushes.LightGreen;
+            toDoItemListGrid.SelectedItem = null;
+                
+        }
+
+        public void OnUnChecked(object sender, RoutedEventArgs e)
+        {
+            var row = toDoItemListGrid.ItemContainerGenerator.ContainerFromItem(toDoItemListGrid.SelectedItem) as DataGridRow;
+            row.Background = Brushes.White;
+            toDoItemListGrid.SelectedItem = null;
+        }
+
     }
 }
